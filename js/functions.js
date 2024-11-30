ce66ff3181a52211bcac143f9b90cae667c233ae
VladisLove K.S. <103459415+SpikeJnR@users.noreply.github.com>
2024-11-30 15:36:57 +0300

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -21,25 +21,25 @@ function getInteger (value) {
 
   return NaN;
 }
-/*
+
 // * Проверка сравнения длинны строки с числом * //
-console.log(isStringLonger('проверяемая строка', 20));
-console.log(isStringLonger('проверяемая строка', 18));
-console.log(isStringLonger('проверяемая строка', 10));
+isStringLonger('проверяемая строка', 20);
+isStringLonger('проверяемая строка', 18);
+isStringLonger('проверяемая строка', 10);
 
 // * Проверка для определения палиндрома * //
-console.log(isPalindrom('топот'));
-console.log(isPalindrom('ДовОд'));
-console.log(isPalindrom('Кекс'));
-console.log(isPalindrom('Лёша на полке клопа нашёл '));
+isPalindrom('топот');
+isPalindrom('ДовОд');
+isPalindrom('Кекс');
+isPalindrom('Лёша на полке клопа нашёл ');
 
 // * Проверка для поиска числа в строке * //
-console.log(getInteger('2023 год'));
-console.log(getInteger('ECMAScript 2022'));
-console.log(getInteger('1 кефир, 0.5 батона'));
-console.log(getInteger('агент 007'));
-console.log(getInteger('а я томат'));
-console.log(getInteger(2023));
-console.log(getInteger(-1));
-console.log(getInteger(1.5));
-*/
+getInteger('2023 год');
+getInteger('ECMAScript 2022');
+getInteger('1 кефир, 0.5 батона');
+getInteger('агент 007');
+getInteger('а я томат');
+getInteger(2023);
+getInteger(-1);
+getInteger(1.5);
+
