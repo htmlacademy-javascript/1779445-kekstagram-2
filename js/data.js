a9d6cae4d0be0689b99206bfe4b715cc31faf436
Vladislav KS <spike.kvs@gmail.com>
2024-12-24 18:47:21 +0300

    module7-task1

--- js/data.js
+++ js/data.js
@@ -1,31 +1,31 @@
 import {getRandomInteger, createRandomValueAtRange} from './util.js';
 import {DESCRIPTIONS, NAMES, COMMENTS} from './const.js';
-import {MIN_VALUE, MAX_VALUE, MIN_LIKES, MAX_LIKES, MAX_COMMENTS_VALUE, MAX_VALUE_AVATAR} from './const.js';
+import {PhotosConst} from './const.js';
 // Генератор массива комментариев //
 function getArrayComments() {
-  const getRandomCommentsId = createRandomValueAtRange(MIN_VALUE, MAX_COMMENTS_VALUE);
+  const getRandomCommentsId = createRandomValueAtRange(PhotosConst.MIN_VALUE, PhotosConst.MAX_COMMENTS_VALUE);
 
   // Создаем экземпляр комментария в качестве объекта //
   const commentObject = () => ({
     id: getRandomCommentsId(),
-    avatar: `img/avatar-${getRandomInteger(MIN_VALUE, MAX_VALUE_AVATAR)}.svg.`,
-    message:  Array.from({length: getRandomInteger(MIN_VALUE, 2)}, () => COMMENTS[createRandomValueAtRange(0, COMMENTS.length - 1)()]).join(' '),
-    name: NAMES[getRandomInteger(MIN_VALUE, NAMES.length)],
+    avatar: `img/avatar-${getRandomInteger(PhotosConst.MIN_VALUE, PhotosConst.MAX_VALUE_AVATAR)}.svg.`,
+    message:  Array.from({length: getRandomInteger(PhotosConst.MIN_VALUE, 2)}, () => COMMENTS[createRandomValueAtRange(0, COMMENTS.length - 1)()]).join(' '),
+    name: NAMES[getRandomInteger(PhotosConst.MIN_VALUE, NAMES.length)],
   });
 
-  return Array.from({length: getRandomInteger(0, MAX_COMMENTS_VALUE)}, commentObject);
+  return Array.from({length: getRandomInteger(0, PhotosConst.MAX_COMMENTS_VALUE)}, commentObject);
 }
 
 //Функция для генерации изображений //
 function createImage(max) {
-  const getRandomImageId = createRandomValueAtRange(MIN_VALUE, MAX_VALUE);
-  const getRandomImageUrl = createRandomValueAtRange(MIN_VALUE, MAX_VALUE);
+  const getRandomImageId = createRandomValueAtRange(PhotosConst.MIN_VALUE, PhotosConst.MAX_VALUE);
+  const getRandomImageUrl = createRandomValueAtRange(PhotosConst.MIN_VALUE, PhotosConst.MAX_VALUE);
 
   const imageObject = () => ({
     id: getRandomImageId(),
     url: `photos/${getRandomImageUrl()}.jpg`,
-    description: DESCRIPTIONS[getRandomInteger(MIN_VALUE, NAMES.length - 1)],
-    likes: getRandomInteger(MIN_LIKES, MAX_LIKES),
+    description: DESCRIPTIONS[getRandomInteger(PhotosConst.MIN_VALUE, NAMES.length - 1)],
+    likes: getRandomInteger(PhotosConst.MIN_LIKES, PhotosConst.MAX_LIKES),
     comments: getArrayComments(),
   });
 
@@ -33,3 +33,4 @@ function createImage(max) {
 }
 
 export {createImage};
+
