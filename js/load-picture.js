5c372a4e4b69640f2eb592ebf536201dac71ed3f
Vladislav KS <spike.kvs@gmail.com>
2025-01-23 21:55:12 +0300

    module12-task2

--- js/load-picture.js
+++ js/load-picture.js
@@ -3,14 +3,21 @@ const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 const fileChooser = document.querySelector('.img-upload__start input[type=file]');
 const preview = document.querySelector('.img-upload__preview img');
 
+let checkType;
+
+
 fileChooser.addEventListener('change', () => {
+  checkType = false;
+
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
 
   const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
 
   if (matches) {
+    checkType = true;
     preview.src = URL.createObjectURL(file);
-
   }
 });
+
+export { checkType };
